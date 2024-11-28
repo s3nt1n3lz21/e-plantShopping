2926cb3d7b9f07ef984b81b2a656cbfae8f7cb85
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-11-28 06:51:31 -0500

    Removed total cart amount above continue shopping button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -76,7 +76,7 @@ const CartItem = ({ onContinueShopping }) => {
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'>{calculateTotalAmount()}</div>
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'>{}</div>
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
